fb2b7cdbb578ff399beacd6028c0193a5c9ed4bd
Krzysztof Feret <96354591+App-Content@users.noreply.github.com>
2022-09-01 21:10:40 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,3 @@ import * as bootstrap from "bootstrap";
 
 import { buttonInitialLoad } from "./buttons";
 import { launchCarousel, closeCarousel } from "./carouselHandle";
-
-fetch(
-  "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fautoelectrogm&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-).then((result) => console.log(result));
